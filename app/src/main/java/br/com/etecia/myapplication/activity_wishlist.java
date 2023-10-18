af60735118ac943ac29e1c167e4aaff9fc75869c
1henri1 <henri135246@gmail.com>
2023-10-17 22:08:14 -0300

    henrique

--- app/src/main/java/br/com/etecia/myapplication/activity_wishlist.java
+++ app/src/main/java/br/com/etecia/myapplication/activity_wishlist.java
@@ -1,14 +1,25 @@
 package br.com.etecia.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_wishlist extends AppCompatActivity {
 
+    BottomNavigationView idBottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wishlist_layout);
+        idBottomBar = findViewById(R.id.idBottomBar);
     }
+
 }
